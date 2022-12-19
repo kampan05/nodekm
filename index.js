737c7c6a8538fbba6c04d2e6070ee069478ba973
kampan05 <120645264+kampan05@users.noreply.github.com>
2022-12-19 12:23:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require ('fs');
+
+//fs.mkdirSync('kampan');
+
+//fs.writeFileSync("kampan/bio.txt", "I am a youtuber");
+
+//fs.appendFileSync("kampan/bio.txt", "I play different games on youtube");
+
+//const data = fs.readFileSync("kampan/bio.txt", "utf-8");
+//console.log(data);
+
+//fs.unlinkSync("kampan/bio.txt");
+
+//fs.rmdirSync("kampan");
+
